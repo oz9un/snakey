88a2e18f985c843b763855c2ab4878e1466edd62
oz9un <57866851+oz9un@users.noreply.github.com>
2020-12-28 18:19:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const details = document.getElementById("lux");
+    
+    // Feature detection
+    if (window.AmbientLightSensor){
+        try{
+          const sensor = new AmbientLightSensor();
+
+          // Detect changes in the light
+          sensor.onreading = () => {
+            details.innerHTML = sensor.illuminance;
+            
+              // Read the light levels in lux 
+              // < 50 is dark room
+              if (sensor.illuminance < 50) {
+                document.body.className = 'darkEnv';
+              } else {
+                document.body.className = 'brightEnv';
+              }
+          }
+          
+          // Has an error occured?
+          sensor.onerror = event => document.getElementById("lux").innerHTML = event.error.message;
+          sensor.start();
+        } catch(err) {
+          details.innerHTML = err.message;
+        }
+    } else {
+      details.innerHTML = 'It looks like your browser doesnt support this feature'; 
+    }
